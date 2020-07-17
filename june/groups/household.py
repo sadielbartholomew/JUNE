cf4a0771515d391ee266c72961c68900c53c4a03
Arnau Quera-Bofarull <arnau.quera-bofarull@durham.ac.uk>
2020-07-17 13:50:47 +0200

    syncing

--- june/groups/household.py
+++ june/groups/household.py
@@ -9,12 +9,16 @@ from june.demography import Person
 
 class Household(Group):
     __slots__ = (
+        "type",
         "area",
         "kid_max_age",
         "young_adult_max_age",
         "adult_max_age",
         "residents",
-        "max_size",
+        "_kids",
+        "_young_adults",
+        "_adults",
+        "_old_adults" "",
     )
 
     class SubgroupType(IntEnum):
@@ -24,15 +28,21 @@ class Household(Group):
         old_adults = 3
 
     def __init__(
-        self, area=None, max_size=np.inf,
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
     ):
         super().__init__()
+        self.spec = "household"
+        self.type = self.get_spec().split("_")[-1]
         self.area = area
         self.kid_max_age = 17
         self.young_adult_max_age = 34
         self.adult_max_age = 64
         self.residents = ()
-        self.max_size = max_size
+        # these are the desired values according to census:
+        self._kids = kids
+        self._young_adults = young_adults
+        self._adults = adults
+        self._old_adults = old_adults
 
     def add(self, person, activity="residence"):
         if person.age <= self.kid_max_age:
@@ -61,6 +71,17 @@ class Household(Group):
             return self.subgroups[self.SubgroupType.adults]
         else:
             return self.subgroups[self.SubgroupType.old_adults]
+
+    @property
+    def max_size(self):
+        attr_names = ["_kids", "_young_adults", "_adults", "_old_adults"]
+        return sum(
+            [
+                getattr(self, name)
+                for name in attr_names
+                if getattr(self, name) is not None
+            ]
+        )
 
     @property
     def kids(self):
@@ -96,40 +117,89 @@ class Household(Group):
 
 
 class HouseholdSingle(Household):
-    def __init__(self, area=None):
-        super().__init__(area=area, max_size=1)
+    def __init__(
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
+    ):
+        super().__init__(
+            area=area,
+            kids=kids,
+            young_adults=young_adults,
+            adults=adults,
+            old_adults=old_adults,
+        )
 
 
 class HouseholdCouple(Household):
-    def __init__(self, area=None):
-        super().__init__(area=area, max_size=2)
+    def __init__(
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
+    ):
+        super().__init__(
+            area=area,
+            kids=kids,
+            young_adults=young_adults,
+            adults=adults,
+            old_adults=old_adults,
+        )
 
 
 class HouseholdFamily(Household):
-    def __init__(self, area=None, n_kids_min=0, max_size=np.inf):
-        super().__init__(area=area, max_size=max_size)
-        self.n_kids_min = n_kids_min
+    def __init__(
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
+    ):
+        super().__init__(
+            area=area,
+            kids=kids,
+            young_adults=young_adults,
+            adults=adults,
+            old_adults=old_adults,
+        )
 
 
 class HouseholdStudent(Household):
-    def __init__(self, area=None, max_size=np.inf):
-        super().__init__(area=area, max_size=max_size)
+    def __init__(
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
+    ):
+        super().__init__(
+            area=area,
+            kids=kids,
+            young_adults=young_adults,
+            adults=adults,
+            old_adults=old_adults,
+        )
 
 
 class HouseholdCommunal(Household):
-    def __init__(self, area=None, max_size=np.inf):
-        super().__init__(area=area, max_size=max_size)
+    def __init__(
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
+    ):
+        super().__init__(
+            area=area,
+            kids=kids,
+            young_adults=young_adults,
+            adults=adults,
+            old_adults=old_adults,
+        )
 
 
 class HouseholdOther(Household):
-    def __init__(self, area=None, max_size=np.inf):
-        super().__init__(area=area, max_size=max_size)
+    def __init__(
+        self, area=None, kids=None, young_adults=None, adults=None, old_adults=None
+    ):
+        super().__init__(
+            area=area,
+            kids=kids,
+            young_adults=young_adults,
+            adults=adults,
+            old_adults=old_adults,
+        )
 
 
 class Households(Supergroup):
     def __init__(self, households: List[Household]):
         super().__init__()
-        self.members = households
 
-    @classmethod
-    def from_household_compositions(household_com
+
+#        self.members = households
+
+# @classmethod
+# def from_household_compositions(household_com
--- test_june/unit/groups/test_households.py
+++ test_june/unit/groups/test_households.py
@@ -60,60 +60,133 @@ class TestHousehold:
 
 class TestHouseholdSingle:
     def test__household_single(self):
-        household = HouseholdSingle(area="test")
+        household = HouseholdSingle(area="test", young_adults=1)
+        assert household._young_adults == 1
+        assert household.spec == "household"
+        assert household.type == "single"
         assert household.area == "test"
-
-
-class TestHouseholdSingle:
-    def test__household_single(self):
-        household = HouseholdSingle(area="test")
-        assert household.area == "test"
+        assert household.max_size == 1
 
 
 class TestHouseholdCouple:
     def test__household_couple(self):
-        household = HouseholdCouple(old_couple=True)
-        assert household.old_couple is True
-        household = HouseholdCouple(old_couple=False)
-        assert household.old_couple is False
+        household = HouseholdCouple(adults=2)
+        assert household._adults == 2
+        assert household.type == "couple"
+        assert household.spec == "household"
+        assert household.max_size == 2
+        household = HouseholdCouple()
 
 
 class TestHouseholdFamily:
     def test__household_family(self):
-        household = HouseholdFamily(
-            n_kids_min=1, single_parent=True, multi_generational=False
-        )
-        assert household.n_kids_min == 1
-        assert household.single_parent is True
-        assert household.multi_generational is False
+        household = HouseholdFamily(kids=1, adults=2)
+        assert household._kids == 1
+        assert household._adults == 2
+        assert household.type == "family"
+        assert household.spec == "household"
+        assert household.max_size == 3
 
 
 class TestHouseholdStudent:
     def test__household_student(self):
         household = HouseholdStudent()
-        assert household.max_size == np.inf
+        assert household.type == "student"
+        assert household.spec == "household"
 
 
 class TestHouseholdCommunal:
     def test__household_student(self):
         household = HouseholdCommunal()
-        assert household.max_size == np.inf
+        assert household.type == "communal"
+        assert household.spec == "household"
 
 
 class TestHouseholdsCreation:
-    def test__households_from_compositions(self):
+    @pytest.fixture(name="households", scope="module")
+    def create_households(self):
         compositions = {
             "single": {1: {"old": True, "number": 10}, 2: {"old": False, "number": 5}},
             "couple": {1: {"old": True, "number": 15}, 2: {"old": False, "number": 7}},
             "families": {
-                1: {"kids": 1, "young_adults" : 0,  "adults": 1, "old_adults": 0, "number": 5},
-                2: {"kids": "2+", "young_adults" : 0, "adults": 2, "old_adults": 0, "number": 4},
-                3: {"kids": 1, "young_adults" : 0, "adults": 2, "old_adults": 1, "number": 3},
-                4: {"kids": 0, "young_adults" : 2, "adults": 2, "old_adults": 0, "number": 2},
+                1: {
+                    "kids": 1,
+                    "young_adults": 0,
+                    "adults": 1,
+                    "old_adults": 0,
+                    "number": 5,
+                },
+                2: {
+                    "kids": "2+",
+                    "young_adults": 0,
+                    "adults": 2,
+                    "old_adults": 0,
+                    "number": 4,
+                },
+                3: {
+                    "kids": 1,
+                    "young_adults": 0,
+                    "adults": 2,
+                    "old_adults": 1,
+                    "number": 3,
+                },
+                4: {
+                    "kids": 0,
+                    "young_adults": 2,
+                    "adults": 2,
+                    "old_adults": 0,
+                    "number": 2,
+                },
             },
-            "students" : {"number" : 10},
-            "communal" : {"number" : 3},
-            "other" : {"kids": 0, "young_adults" : "2+", "adults": "1+", "old_adults": 1, "number": 2}
+            "students": {"number": 10},
+            "communal": {"number": 3},
+            "other": {
+                "kids": 0,
+                "young_adults": "2+",
+                "adults": "1+",
+                "old_adults": 1,
+                "number": 2,
+            },
         }
         households = Households.from_household_compositions(compositions)
+        return households
+
+    def test__number_of_households(self, households):
         assert len(households) == 5 + 7 + 5 + 4 + 3 + 2 + 10 + 3 + 2
+
+    def test__households_from_compositions(self, households):
+        family_households = [
+            household for household in households if household.type == "family"
+        ]
+        assert len(family_households) == 5 + 4 + 3 + 2
+        kids = np.zeros(10)
+        young_adults = np.zeros(10)
+        adults = np.zeros(10)
+        old_adults = np.zeros(10)
+        for household in households:
+            if household._kids == 0:
+                kids[0] += 1
+            elif household._kids == 1:
+                kids[1] += 1
+            else:
+                kids[2] += 1
+            if household._adults == 1:
+                adults[1] += 1
+            elif household._adults == 2:
+                adults[2] += 1
+
+            if household._old_adults == 0:
+                old_adults[1] += 1
+
+        assert kids[0] == 2
+        assert kids[1] == 8
+        assert kids[2] == 4
+
+        assert adults[1] == 5
+        assert adults[2] == 5
+
+        assert old_adults[0] == 11
+        assert old_adults[1] == 3
+
+
+
